9f2cdd03aec08e4fe4960fa262eb11578b416a96
VIRUPAKSHI <139473614+V-Code-S@users.noreply.github.com>
2023-07-31 00:12:52 +0530

    Delete New folder directory

--- New folder/IMAGE 2 PDF/im2.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-
-image_1 = Image.open(r'C:\Users\viruk\OneDrive\Documents\images\viru.png')
-im_1 = image_1.convert('RGB')
-im_1.save(r'C:\Users\viruk\OneDrive\Documents\pdf123.pdf')
